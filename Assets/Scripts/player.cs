fbe60af77ff4055e327cf1e04e8f2686fdbb8794
jiany1 <yao71@msn.com>
2016-10-31 14:11:07 -0700

    player object

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+    private int health;
+    private int mana;
+
+    public player (int iHealth, int iMana)
+    {
+        health = iHealth;
+        mana = iMana;
+    }
+
+    public bool isAlive()
+    {
+        return health > 0;
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public void gainMana(int gMana)
+    {
+        mana += gMana;
+    }
+
+    public int healthLevel()
+    {
+        return health;
+    }
+
+    public int manaLevel()
+    {
+        return mana;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
