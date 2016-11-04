a80062d968d1b5d904c3b46b8e7231b9e53d8aa2
tony <tony.ortega715@gmail.com>
2016-11-03 21:57:25 -0700

    do damage function

---
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+    public static int do_damage(double chance, int damage)
+    {
+        if (Random.value > chance)
+            return 0;
+        else
+            return damage;
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
