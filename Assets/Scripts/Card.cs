152fc396c2cc2398c9c7fbfe98569082d7853f77
Xatoe <ryanhd@uci.edu>
2016-11-10 20:36:07 -0800

    Implemented Card Class
    Card class created

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+	private string name;
+	private double chance;
+	private int damage;
+	private int pip;
+
+
+	/* Constructor */
+	public Card (string name1, double chance1, int damage1, int pip1)
+	{
+		name = name1;
+		chance = chance1;
+		damage = damage1;
+		pip = pip1;
+	}
+	/* Command returns name */
+	public string getName ()
+	{
+		return name;
+	}
+	/* Command returns chance */
+	public double getChance()
+	{
+		return chance;
+	}
+	/* Command returns Damage */
+	public int getDamage()
+	{
+		return damage;
+	}
+	/* Command returns pip cost */
+	public int getPip()
+	{
+		return pip;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
