6222f19d896d1195089be4c109ea3a6ec00bf68d
tony <tony.ortega715@gmail.com>
2016-11-17 21:01:53 -0800

    return to title screen button

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;//load a scence ";" at the end of a line
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public void start()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
---
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour {
+
+	public void start()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
